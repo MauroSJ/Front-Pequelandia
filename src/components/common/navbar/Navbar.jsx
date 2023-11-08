bdfee2e88a6137d0816c4e4c25a5517b7f7b7875
MauroSJ <mauro10z@hotmail.com>
2023-11-08 20:33:55 -0300

    modifying navbar mobile

--- src/components/common/navbar/Navbar.jsx
+++ src/components/common/navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -6,17 +6,19 @@ import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
+import SearchIcon from '@mui/icons-material/Search';
 import Container from '@mui/material/Container';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import Tooltip from '@mui/material/Tooltip';
 import MenuItem from '@mui/material/MenuItem';
+import AdbIcon from '@mui/icons-material/Adb';
 
-const pages = ['Productos', 'Precios', 'Nosotros'];
-const settings = ['Perfil', 'Cuenta', 'Cerrar sesion'];
 
-const Navbar = () => {
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
 
+function ResponsiveAppBar() {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
   const [anchorElUser, setAnchorElUser] = React.useState(null);
 
@@ -38,19 +40,37 @@ const Navbar = () => {
   return (
     <AppBar position="static">
       <Container maxWidth="xl">
-        <Toolbar disableGutters>
-          <img src="" width={"50px"} height={"50px"} alt="" />
+        <Toolbar disableGutters sx={{justifyContent:'space-between'}}>
+          <AdbIcon sx={{ display: { xs: 'none', md: 'flex' }, mr: 1, }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="Pequelandia"
+            sx={{
+              mr: 2,
+              display: { xs: 'none', md: 'flex' },
+              fontFamily: 'monospace',
+              fontWeight: 700,
+              letterSpacing: '.3rem',
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            LOGO
+          </Typography>
 
-          <Box sx={{ flexGrow: 1, display: { xs: 'flex', md: 'none' } }}>
+          <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
             <IconButton
               size="large"
               aria-label="account of current user"
               aria-controls="menu-appbar"
-              aria-haspopup="true"
               onClick={handleOpenNavMenu}
               color="inherit"
+              sx={{padding:'0'}}
             >
-              <MenuIcon />
+            <MenuIcon />
+            <img src="https://img.freepik.com/vector-gratis/logotipo-creatividad-pluma_1057-2531.jpg?w=30&t=st=1699482425~exp=1699483025~hmac=df281e5a6175ddaa73344aa7408c38a0c885c6bdc25e2a367d001d6c8ddc399b" height={30} alt="Logo Pequelandia" />
             </IconButton>
             <Menu
               id="menu-appbar"
@@ -77,24 +97,11 @@ const Navbar = () => {
               ))}
             </Menu>
           </Box>
-          <Typography
-            variant="h5"
-            noWrap
-            component="a"
-            href="#app-bar-with-responsive-menu"
-            sx={{
-              mr: 2,
-              display: { xs: 'flex', md: 'none' },
-              flexGrow: 1,
-              fontFamily: 'monospace',
-              fontWeight: 700,
-              letterSpacing: '.3rem',
-              color: 'inherit',
-              textDecoration: 'none',
-            }}
-          >
-            LOGO
-          </Typography>
+
+          <IconButton color="inherit">
+              <SearchIcon/>
+            </IconButton>
+          
           <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
             {pages.map((page) => (
               <Button
@@ -110,7 +117,7 @@ const Navbar = () => {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="" />
+                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
               </IconButton>
             </Tooltip>
             <Menu
@@ -139,7 +146,6 @@ const Navbar = () => {
         </Toolbar>
       </Container>
     </AppBar>
-  )
+  );
 }
-
-export default Navbar
+export default ResponsiveAppBar;
