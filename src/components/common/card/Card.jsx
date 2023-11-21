8df6bc303fc1ae76b6d699ebd3f1f004490a6da1
MauroSJ <mauro10z@hotmail.com>
2023-11-21 19:10:00 -0300

    Custumizing card

--- src/components/common/card/Card.jsx
+++ src/components/common/card/Card.jsx
@@ -1,32 +1,56 @@
-import React from 'react'
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import { CardActionArea } from '@mui/material';
+import React from "react";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import { CardActionArea } from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import { Button } from "@mui/material";
+import Box from "@mui/material/Box";
+import bodyImage from "../../../assets/bodysinfondo.png";
 
 const Shopcard = () => {
   return (
-    <Card sx={{ maxWidth: 345 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          alt="green iguana"
-        />
+    <>
+      <Card sx={{ maxWidth: 345, height: 345 }}>
+        <Box>
+          <CardActionArea sx={{ display: "flex" }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={bodyImage}
+              alt="green iguana"
+              sx={{ height: "200px", width: "auto" }}
+            />
+          </CardActionArea>
+
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              mx: "12px",
+            }}
+          >
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton>
+              <ShoppingCartIcon />
+            </IconButton>
+          </Box>
+        </Box>
+
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            Lizard
-          </Typography>
+          <Button sx={{ padding: "0" }}>Lizard</Button>
           <Typography variant="body2" color="text.secondary">
-            Lizards are a widespread group of squamate reptiles, with over 6,000
-            species, ranging across all continents except Antarctica
+            <span>$300</span>
           </Typography>
         </CardContent>
-      </CardActionArea>
-    </Card>
-  )
-}
+      </Card>
+    </>
+  );
+};
 
-export default Shopcard
+export default Shopcard;
